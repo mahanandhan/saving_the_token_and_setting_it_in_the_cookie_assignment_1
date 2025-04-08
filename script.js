aa6f4c5ff765143257b1704780e5bb734e2fe876
mahanandhan <mahanandhanmanish@gmail.com>
2025-04-08 12:06:25 +0530

    added

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'mysecret';
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  try {
+    const token = jwt.sign(payload, SECRET_KEY , {expiresIn: '1h'});
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    return decoded;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
